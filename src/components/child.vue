3720028c27d66b09f079bf31bc8954f8893e5e91
jun.lin <67188543+jim55167@users.noreply.github.com>
2022-11-03 12:03:04 +0800

    FirstUpdate

---
+++ src/components/child.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const state = ref([]);
+
+const props = defineProps({
+  arr: {
+    type: Array,
+    required: true,
+  },
+});
+// console.log(state)
+// console.log(props)
+// console.log(props.arr)
+
+function getData() {
+  const data = props.arr;
+  // console.log(data)
+  data.forEach((item) => {
+    state.value.push({
+      "score": item.score,
+      "id": item.show.id,
+      "name": item.show.name
+    });
+  });
+}
+
+getData();
+</script>
+
+<template>
+  <div>
+    <ul v-for="states in state" :key="state.score">
+      <li>Name：{{states.name}}</li>
+      <li>ID：{{states.id}}</li>
+      <li>Score: {{states.score}}</li>
+    </ul>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, inject, onMounted } from "vue";
+import child from "@/components/child.vue";
+const axios = inject('axios')
+
+const resData = ref([]);
+
+onMounted(async () => {
+  await axios.get("https://api.tvmaze.com/search/shows?q=girls")
+    .then(res => {
+      resData.value = res.data
+    })
+    .catch(err => {
+      console.log("Error：" + err);
+    })
+})
+
+</script>
+
+<template>
+  <div>
+    <child :arr="resData" />
+  </div>
+</template>
